5fb9d774e5383271008f9df9849bae4517c61862
Ralph <ralph@rzm.at>
2018-05-17 21:09:43 -0700

    Switch Excel output to xlsx

--- lib/dossier/xls.rb
+++ lib/dossier/xls.rb
@@ -1,64 +1,57 @@
+require 'write_xlsx'
+
 module Dossier
   class Xls
     attr_reader :report, :headers, :collection
 
-    HEADER = %Q{<?xml version="1.0" encoding="UTF-8"?>\n<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40">\n<Worksheet ss:Name="Sheet1">\n<Table>\n}
-    FOOTER = %Q{</Table>\n</Worksheet>\n</Workbook>\n}
-
     def initialize(report)
       @report = report
-      @headers    = report.columns.select{|col| col unless report.format_header(col).blank? }
+      @headers = report.columns.select { |col| col unless report.format_header(col).blank? }
       @collection = report.raw_results
+      @string_buffer = StringIO.new
+      @workbook = WriteXLSX.new(@string_buffer)
+      @worksheet = @workbook.add_worksheet
     end
 
     def each
-      yield HEADER
-      yield headers_as_row
-      @collection.each { |record| yield as_ar_row(record) }
-      yield FOOTER
+      add_header_row
+      @collection.each_with_index { |record, row_index| as_ar_row(record, row_index + 1) }
+      @workbook.close
+      yield @string_buffer.string
     end
 
     private
 
-    def as_cell(el)
-      if el.kind_of?(Array)
-        %{<Cell><Data ss:Type="String">#{el.map(&:to_s).join(', ')}</Data></Cell>}
-      elsif el.is_a?(DateTime)
-        %{<Cell><Data ss:Type="DateTime">#{el}</Data></Cell>}
-      elsif el.is_a?(Float) || el.is_a?(Integer) || el.is_a?(BigDecimal)
-        %{<Cell><Data ss:Type="Number">#{el}</Data></Cell>}
-      else
-        %{<Cell><Data ss:Type="String">#{CGI::escapeHTML(el.to_s)}</Data></Cell>}
-      end
-    end
-
-    def as_ar_row(row)
-      "<Row>\n" +  headers.collect{|column|
-
-        args = [column]        
-        if (row.method(column).arity == -1  rescue false)
+    def as_ar_row(row, row_index)
+      headers.each_with_index do |column, column_index|
+        args = [column]
+        if begin
+              row.method(column).arity == -1
+            rescue
+              false
+            end
            args << report.options
         end
         value = row.public_send(*args)
 
-        #use export formatter for this cell, if we have one
-        if report.respond_to?("export_format_#{column}") 
+        # use export formatter for this cell, if we have one
+        if report.respond_to?("export_format_#{column}")
           args = ["export_format_#{column}", value, row]
-          value = report.public_send(*args) 
+          value = report.public_send(*args)
         end
-          
-        as_cell(value)
 
-      }.join("\n") + "\n</Row>\n"
+        value = value.map(&:to_s).join(', ') if value.is_a?(Array)
+        @worksheet.write(row_index, column_index, value)
+      end
     end
 
-    def as_row(array)
-      my_array = array.map{|a| as_cell(a)}.join("\n")
-      "<Row>\n" + my_array + "\n</Row>\n"
-    end
+    def add_header_row
+      header_format = @workbook.add_format
+      header_format.set_bold
 
-    def headers_as_row
-      as_row(@headers.map { |header| report.format_header(header) })
+      @headers.each_with_index do |header, column_index|
+        @worksheet.write(0, column_index, report.format_header(header), header_format)
+      end
     end
   end
 end
